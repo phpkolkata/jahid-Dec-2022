cfd8ec6ac366ab2169d9c7de6e63855ce74be9f7
Md Asif Iqbal <phpkolkata@gmail.com>
2023-05-09 08:31:16 +0530

    class27

--- PHP/17.php
+++ PHP/17.php
@@ -1,11 +1,33 @@
 <?php
-$options = 5;
-
-for($i=1;$i<=$options;$i++){
-    echo $i;
-}
-
 // hw: loop to bring ul li listing as per $options (try all 3 loops)
 
+$options = 15;
 
+print "<ol>";
+for ($i = 1; $i <= $options; $i++) {
+    echo "<li>Option $i</li>";
+}
+print "</ol>";
 
+print "<select>";
+for ($i = 1; $i <= $options; $i++) {
+    echo "<option>Option $i</option>";
+}
+print "</select>";
+
+print "<table border='1'>";
+for ($i = 1; $i <= $options; $i++) {
+    echo "<tr>
+    <td>Option $i</td>
+    </tr>";
+}
+print "</table>";
+
+?>
+
+<!-- <select>
+    <option>Option 1</option>
+    <option>Option 1</option>
+    <option>Option 1</option>
+    <option>Option 1</option>
+</select> -->
---
+++ PHP/18.php
@@ -0,0 +1,107 @@
+<?php
+// Functions - set of code to reduce complexity and shorten the programm.
+// user-defined & pre-defined
+
+// 1. function name
+// 2. function params/args
+// 3. optional args/params
+// 4. return / process
+// 5. return type
+//----------------------------------
+
+// processed function
+function hi(): void
+{
+    echo "hi";
+}
+hi();
+//----------------------------------
+// returned function
+function hello(): string
+{
+    return "hello";
+}
+echo hello();
+//----------------------------------
+
+// params
+function getCoffee($milk, $sugar, $cofee): int
+{
+    return $milk + $sugar + $cofee;
+}
+echo getCoffee(2, 6, 4);
+//------------------------------------------------------
+
+// why function?
+// $options = 6;
+// print "<select>";
+// for ($i = 1; $i <= $options; $i++) {
+//     echo "<option>Option $i</option>";
+// }
+// print "</select>";
+
+function generateDropDown($options)
+{
+    print "<select>";
+    for ($i = 1; $i <= $options; $i++) {
+        echo "<option>Option $i</option>";
+    }
+    print "</select>";
+
+}
+
+generateDropDown(20);
+generateDropDown(20);
+generateDropDown(20);
+generateDropDown(20);
+generateDropDown(20);
+generateDropDown(20);
+generateDropDown(20);
+
+//------------------------------------------------------
+
+function add($v1, $v2): int
+{
+    $total = $v1 + $v2;
+    return $total;
+}
+
+function sub($v1, $v2): int
+{
+    $total = $v1 - $v2;
+    return $total;
+}
+
+function mul($v1, $v2): int
+{
+    $total = $v1 * $v2;
+    return $total;
+}
+
+function div($v1, $v2): int
+{
+    $total = $v1 / $v2;
+    return $total;
+}
+
+$total = add(30, 55);
+echo $total;
+
+//----------------------------------------
+
+// Function with multiple operation using params
+function calc($v1, $v2, $act)
+{
+    if ($act == "+") {
+        return $v1 + $v2;
+    } else if ($act == "-") {
+        return $v1 - $v2;
+    } else if ($act == "*") {
+        return $v1 * $v2;
+    } else if ($act == "/") {
+        return $v1 / $v2;
+    }
+}
+
+echo calc(30, 40, "sub"); //+,add  -,sub  *,mul  /,div
+// echo calc(30, 40);  // default nature to add
