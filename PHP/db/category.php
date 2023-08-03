ebe0dfeb0db8f591a7d51b82eb9878580517b50b
Md Asif Iqbal <phpkolkata@gmail.com>
2023-08-03 08:24:18 +0530

    pagination & search

--- PHP/db/category.php
+++ PHP/db/category.php
@@ -12,8 +12,30 @@
 
 <?php
 require "connection.php";
-$sql = "select * from category";
+// Search
+$where = "";
+if (isset($_REQUEST['srch'])) {
+    $where = "WHERE `name` like '%$_REQUEST[srch]%'";
+}
+
+// Count Record
+$sql = "select * from category $where";
 $res = mysqli_query($con, $sql);
+$tot = mysqli_num_rows($res); //counts number of rows in $res
+
+$limit = 4;
+$pages = ceil($tot / $limit);
+// echo $pages;
+$start = 0;
+if (isset($_REQUEST['p'])) {
+    $start = ($_REQUEST['p'] - 1) * $limit;
+}
+
+// Main Query
+$sql = "select * from category $where LIMIT $start,$limit";
+$res = mysqli_query($con, $sql);
+
+print $sql;
 
 if (isset($_REQUEST['msg'])) {
     $msg = strtoupper($_REQUEST['msg']);
@@ -21,6 +43,11 @@ if (isset($_REQUEST['msg'])) {
 }
 
 print "<div class='mx-auto w-50 mt-5 mb-1 clearfix'>
+<form action='category.php' method='get'>
+<input type='search' class='float-start' name='srch'>
+<button class='btn btn-warning float-start'>Search</button>
+</form>
+
 <a href='add-category.php' class='btn btn-primary float-end'>Add Category</a>
 </div>";
 
@@ -49,7 +76,20 @@ while ($row = mysqli_fetch_assoc($res)) {
 }
 
 print "</table>";
+
 ?>
+<div class="mx-auto w-50">
+  <ul class="pagination">
+    <?php
+for ($i = 1; $i <= $pages; $i++) {
+    print "<li class='page-item'><a class='page-link' href='?p=$i'>
+        $i
+    </a></li>";
+}
+?>
+  </ul>
+</div>
+
 </body>
 </html>
 
